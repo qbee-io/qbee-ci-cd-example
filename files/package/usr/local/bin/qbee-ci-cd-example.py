c169b62e92ee01e58930e327e8186a690ea1a537
jonhenrik13 <jonhenrik@gmail.com>
2022-05-12 11:47:24 +0200

    Add reading of config file

--- files/package/usr/local/bin/qbee-ci-cd-example.py
+++ files/package/usr/local/bin/qbee-ci-cd-example.py
@@ -1,12 +1,20 @@
 #!/usr/bin/env python3
 
 from flask import Flask
+import yaml
+from yaml.loader import SafeLoader
+
+config_file = "/etc/qbee-ci-cd-example/qbee-ci-cd-config.yml"
+
+# Open the file and load the file
+config = {}
+with open(config_file) as f:
+  config = yaml.load(f, Loader=SafeLoader)
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-  return "<p>Hello, World!</p>"
+  return "<p>{}</p>".format(config["title"])
 
 app.run()
-
